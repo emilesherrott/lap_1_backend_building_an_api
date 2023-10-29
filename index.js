d0a56cd47b0d7116f6068f2007e46c239d74e59d
“emilesherrott” <emilesherrott@gmail.com>
2023-10-29 11:23:04 +0000

    new code

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const express = require("express")
+const cors = require("cors")
 const fs = require("fs")
 const app = express()
 const port = 3000
@@ -7,6 +8,7 @@ const port = 3000
 const logger = require("./logger")
 
 app.use(express.json())
+app.use(cors())
 app.use(logger)
 
 // ! DATA IMPORT
